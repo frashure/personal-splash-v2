8b9c738745ad6ac31d8cde65a4d49b0995546a8e
Chris Frashure <csfrashure@gmail.com>
2018-10-02 18:44:38 -0400

    Removed JavaScript code from Index; created script file

---
+++ script.js
@@ -0,0 +1,24 @@
+  function dropDown() {
+    document.getElementById("dropdownMenu").classList.toggle("show");
+    console.log("DropDown success!");
+  }
+  function menuAway() {
+    document.getElementById("dropdownMenu").classList.toggle("show");
+    console.log("DropDown success!");
+  }
+  function undo() {
+    document.getElementById("name_title").innerHTML = "chris.frashure";
+    console.log("DropDown success!");
+  }
+  function words() {
+    document.getElementById("name_title").innerHTML = "my thoughts on all of the things";
+    console.log("DropDown success!");
+  }
+  function photo() {
+    document.getElementById("name_title").innerHTML = "cool looking things";
+        console.log("DropDown success!");
+  }
+  function connect() {
+    document.getElementById("name_title").innerHTML = "find me on the net";
+        console.log("DropDown success!");
+  }
